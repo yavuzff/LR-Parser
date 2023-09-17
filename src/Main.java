059ce5bb3f06ae0d635c8e9094493819ed1dc066
yavuzff <75276351+yavuzff@users.noreply.github.com>
2023-09-17 22:33:15 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// convert infix/prefix/postfix to purely postfix notation
+// use stack based approach to pop and evaluate easily
+// leafs are operands, branches formed by operators
